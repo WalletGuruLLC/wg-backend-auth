dca1b0f15188619ca2b50142792af97a1f309d41
danielordonezg <danielale.gomez@correo.usa.edu.co>
2024-08-01 08:02:27 -0500

    Add swagger documentation

--- src/api/user/controller/user.controller.ts
+++ src/api/user/controller/user.controller.ts
@@ -16,38 +16,69 @@ import { SignInDto } from '../dto/signin.dto';
 import { AuthConfirmPasswordUserDto } from '../dto/auth-confirm-password-user.dto';
 import { AuthForgotPasswordUserDto } from '../dto/auth-forgot-password-user.dto';
 import { AuthChangePasswordUserDto } from '../dto/auth-change-password-user.dto';
+import {
+	ApiCreatedResponse,
+	ApiForbiddenResponse,
+	ApiOkResponse,
+	ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	@Post()
+	@ApiCreatedResponse({
+		description: 'The record has been successfully created.',
+	})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	create(@Body() createUserDto: CreateUserDto) {
 		return this.userService.create(createUserDto);
 	}
 
 	@Get(':id')
+	@ApiOkResponse({
+		description: 'The record has been successfully retrieved.',
+	})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	findOne(@Param('id') id: string) {
 		return this.userService.findOne(id);
 	}
 
 	@Patch(':id')
+	@ApiOkResponse({
+		description: 'The record has been successfully updated.',
+	})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
 		return this.userService.update(id, updateUserDto);
 	}
 
 	@Delete(':id')
+	@ApiOkResponse({
+		description: 'The record has been successfully deleted.',
+	})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	remove(@Param('id') id: string) {
 		return this.userService.remove(id);
 	}
 
 	@Post('signin')
+	@ApiOkResponse({
+		description: 'The user has been successfully signed in.',
+	})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	signin(@Body() signinDto: SignInDto) {
 		return this.userService.signin(signinDto);
 	}
 
 	@Post('/change-password')
 	@UsePipes(ValidationPipe)
+	@ApiOkResponse({
+		description: 'The password has been successfully changed.',
+	})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	async changePassword(
 		@Body() authChangePasswordUserDto: AuthChangePasswordUserDto
 	) {
@@ -62,6 +93,10 @@ export class UserController {
 
 	@Post('/forgot-password')
 	@UsePipes(ValidationPipe)
+	@ApiOkResponse({
+		description: 'The password reset request has been successfully processed.',
+	})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	async forgotPassword(
 		@Body() authForgotPasswordUserDto: AuthForgotPasswordUserDto
 	) {
@@ -70,6 +105,10 @@ export class UserController {
 
 	@Post('/confirm-password')
 	@UsePipes(ValidationPipe)
+	@ApiOkResponse({
+		description: 'The password has been successfully confirmed.',
+	})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	async confirmPassword(
 		@Body() authConfirmPasswordUserDto: AuthConfirmPasswordUserDto
 	) {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { dynamoConnect } from './config/dbconfig';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	await dynamoConnect();
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Cats example')
+		.setDescription('The cats API description')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
+
 	await app.listen(3000);
 }
 bootstrap();
