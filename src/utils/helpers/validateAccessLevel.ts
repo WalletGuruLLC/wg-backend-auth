0423e7938de3c97e1a93545688408f487eaf346a
danielordonezg <danielale.gomez@correo.usa.edu.co>
2024-09-03 16:34:54 -0500

    fix validation number

--- src/utils/helpers/validateAccessLevel.ts
+++ src/utils/helpers/validateAccessLevel.ts
@@ -1,3 +1,7 @@
 export function isNumberInRange(num) {
-	return typeof num === 'number' && !isNaN(num) && num >= 8 && num <= 15;
+	return (
+		typeof num === 'number' &&
+		!isNaN(num) &&
+		(num === 0 || (num >= 8 && num <= 15))
+	);
 }
