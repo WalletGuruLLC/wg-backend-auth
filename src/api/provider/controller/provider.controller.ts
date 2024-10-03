12547b9cf8131df1287bcfcfcdd814935c5ea905
Elvin Javier <elvin.javier@scrummers.co>
2024-10-03 09:36:05 -0400

    Fixing responses

--- src/api/provider/controller/provider.controller.ts
+++ src/api/provider/controller/provider.controller.ts
@@ -431,15 +431,17 @@ export class ProviderController {
 				id
 			);
 			if (provider?.customCode) {
-				return {
-					customCode: provider?.customCode,
-				};
+				return res.status(HttpStatus.UNAUTHORIZED).send({
+					statusCode: HttpStatus.UNAUTHORIZED,
+					customCode: 'WGE0131',
+				});
 			}
-			return {
+
+			return res.status(HttpStatus.OK).send({
 				statusCode: HttpStatus.OK,
 				customCode: 'WGS0034',
 				data: provider,
-			};
+			});
 		} catch (error) {
 			console.log('error', error?.message);
 			Sentry.captureException(error);
