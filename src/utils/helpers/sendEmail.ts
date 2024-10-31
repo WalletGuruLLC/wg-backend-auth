669e9821edc7ed8d4eff5b53e4f39783a8cf146d
Cristian <christian.dulcey@gmail.com>
2024-10-31 11:26:59 -0500

    chage name env

--- src/utils/helpers/sendEmail.ts
+++ src/utils/helpers/sendEmail.ts
@@ -8,7 +8,7 @@ export const sendEmail = async (
 ): Promise<void> => {
 	const ses = new AWS.SES({
 		region: process.env.AWS_REGION,
-		accessKeyId: process.env.AWS_KEY_ID,
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	});
 
