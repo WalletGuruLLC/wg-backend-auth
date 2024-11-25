5450d5272e53fe27067158ccaa2ede54832e44f2
danielordonezg <danielale.gomez@correo.usa.edu.co>
2024-11-25 17:03:15 -0500

    fix lint

--- src/api/health/health.module.ts
+++ src/api/health/health.module.ts
@@ -1,4 +1,10 @@
-import { forwardRef, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+	forwardRef,
+	MiddlewareConsumer,
+	Module,
+	NestModule,
+	RequestMethod,
+} from '@nestjs/common';
 import { HealthController } from './controller/health.controller';
 import { AccessControlMiddleware } from '../user/guard/access-control-guard';
 import { ConfigModule } from '@nestjs/config';
@@ -9,23 +15,24 @@ import { UserModule } from '../user/user.module';
 import { ProviderController } from '../provider/controller/provider.controller';
 
 @Module({
-  imports: [
-    ConfigModule,
-    forwardRef(() => UserModule),
-    forwardRef(() => RoleModule),
-  ],
-  controllers: [HealthController],
-  providers: [HealthService,CognitoAuthGuard],
-  exports: [HealthService],
+	imports: [
+		ConfigModule,
+		forwardRef(() => UserModule),
+		forwardRef(() => RoleModule),
+	],
+	controllers: [HealthController],
+	providers: [HealthService, CognitoAuthGuard],
+	exports: [HealthService],
 })
 export class HealthModule implements NestModule {
-  usersPath = 'api/v1/health-check';
+	usersPath = 'api/v1/health-check';
 
-  configure(consumer: MiddlewareConsumer,) {
-    consumer.apply(AccessControlMiddleware).forRoutes(
-      { path: `${this.usersPath}/uptime`, method: RequestMethod.GET },
-    );
-  }
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(AccessControlMiddleware).forRoutes({
+			path: `${this.usersPath}/uptime`,
+			method: RequestMethod.GET,
+		});
+	}
 }
 
-console.log('HealthModule',ProviderController);
+console.log('HealthModule', ProviderController);
