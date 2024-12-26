9fca0c0e536ab66f4098dcfc3e4289bb4009b03e
Cristian <christian.dulcey@gmail.com>
2024-12-26 14:44:07 -0500

    fix update role access

--- src/utils/helpers/validateAccessLevel.ts
+++ src/utils/helpers/validateAccessLevel.ts
@@ -1,7 +1,3 @@
 export function isNumberInRange(num) {
-	return (
-		typeof num === 'number' &&
-		!isNaN(num) &&
-		(num === 0 || (num >= 8 && num <= 15))
-	);
+	return typeof num === 'number' && !isNaN(num) && (num === 0 || num <= 15);
 }
